25b27c54bc423c5745f8f7e39b112372c797d8bf
Daniele Di Sarli <danieleds0@gmail.com>
2022-09-02 18:38:24 +0200

    simple demo

--- browser-sdk/video/constructor/app.js
+++ browser-sdk/video/constructor/app.js
@@ -2,22 +2,35 @@ import './styles.css';
 import { DEMO_TOKEN } from './constants';
 import * as SignalWire from '@signalwire/js'
 
+const btnJoin = document.getElementById('btnJoin')
+const btnLeave = document.getElementById('btnLeave')
+
 // Initialize the video
-async function connect() {
+async function join() {
+  // Configure the room session
   const roomSession = new SignalWire.Video.RoomSession({
     token: DEMO_TOKEN,
     rootElement: document.getElementById("myVideoElement"),
   });
 
+  // Set up event listeners
   roomSession.on("member.joined", (e) => {
     console.log(`${e.member.name} joined`);
   });
 
-  document.getElementById('btnLeave').addEventListener('click', () => {
-    roomSession.leave()
-  })
+  // Disconnect when clicking on the "Leave" button
+  btnLeave.onclick = async () => {
+    await roomSession.leave()
+    btnJoin.style.display = 'block'
+    btnLeave.style.display = 'none'
+  }
 
-  roomSession.join();
+  btnJoin.style.display = 'none'
+
+  // After everything is set up, join the room.
+  await roomSession.join();
+
+  btnLeave.style.display = 'block'
 }
 
-connect()
+btnJoin.addEventListener('click', join)
